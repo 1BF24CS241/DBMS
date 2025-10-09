fee3eabe7ac99a6bd06e349eb97d4a476ff824dc
1BF24CS241 <rajvardhan.cs24@bmsce.ac.in>
2025-10-09 09:41:20 +0530

    Delete query 1.sql

--- query 1.sql
+++
@@ -1,8 +0,0 @@
-select accident_date, location from accident ;
-update participated set damage_amount = 25000 where reg_num = 'KA053408' and report_num = 14 ;
-select * from participated ;
-
-insert into accident values ( '16' , '2008-03-08' , 'ashok nagar');
-select *from accident;
-
-select driver_id from participated where damage_amount >=25000 ;
